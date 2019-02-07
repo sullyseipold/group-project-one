a1b64d00d3c4aded01a691089a50bc0f344333f7
mirwoz <miranda.wozmak@gmail.com>
2019-02-06 20:35:15 -0500

    Added Google Maps API request for travel time

--- assets/javascript/yaysnow.js
+++ assets/javascript/yaysnow.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-  console.log("ready");
 
-  var liftieUrl = "http://liftie.info/api/resort/";
+  console.log("ready");
 
   // Initialize Firebase
   var config = {
@@ -20,61 +19,102 @@ $(document).ready(function () {
   // the function will either add to the searchCount or add the mountain as needed
   $("#searchButton").on("click", function (event) {
     console.log("I've been clicked!");
+    gettravelTime();
 
-    // var mountain = $('#mountainInput').val();
+    var searchTerm = $("#mountainInput").val().trim();
+    var queryURL = "https://in-the-snow.firebaseio.com/searches/" + searchTerm + ".json"
+    $.get(queryURL).then(function (response) {
+      if (response != null) {
+        var count = response.count;
+        count++
+        console.log(count);
 
-    // getLiftieReport(mountain);
-
-    // var searchTerm = $("#mountainInput").val().trim();
-    // var queryURL = "https://in-the-snow.firebaseio.com/searches/" + searchTerm + ".json"
-    // $.get(queryURL).then(function (response) {
-    //   if (response != null) {
-    //     var count = response.count;
-    //     count++
-    //     console.log(count);
-
-    //     $.ajax({
-    //       type: 'PUT',
-    //       url: queryURL,
-    //       contentType: 'application/json',
-    //       data: JSON.stringify({
-    //         "count": count,
-    //       })
-    //     }).done(function () {
-    //       console.log('SUCCESS');
-    //     }).fail(function (msg) {
-    //       console.log('FAIL');
-    //     }).always(function (msg) {
-    //       console.log('ALWAYS');
-    //     });
-    //   } else {
-    //     $.ajax({
-    //       type: 'PUT',
-    //       url: queryURL,
-    //       contentType: 'application/json',
-    //       data: {
-    //         count: count,
-    //       }
-    //     }).done(function () {
-    //       console.log('SUCCESS');
-    //     }).fail(function (msg) {
-    //       console.log('FAIL');
-    //     }).always(function (msg) {
-    //       console.log('ALWAYS');
-    //     });
-    //   }
-    // });
-  });
-
-  function getLiftieReport(mountain) {
-
-    if (mountain != null && mountain != "") {
-    
-        $.get(liftieUrl + mountain).then(function(response) {
-            
-            console.log(response);
+        $.ajax({
+          type: 'PUT',
+          url: queryURL,
+          contentType: 'application/json',
+          data: JSON.stringify({
+            "count": count,
+          })
+        }).done(function () {
+          console.log('SUCCESS');
+        }).fail(function (msg) {
+          console.log('FAIL');
+        }).always(function (msg) {
+          console.log('ALWAYS');
+        });
+      } else {
+        $.ajax({
+          type: 'PUT',
+          url: queryURL,
+          contentType: 'application/json',
+          data: {
+            count: count,
+          }
+        }).done(function () {
+          console.log('SUCCESS');
+        }).fail(function (msg) {
+          console.log('FAIL');
+        }).always(function (msg) {
+          console.log('ALWAYS');
         });
       }
-  };
+    });
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //request for google maps
+  function gettravelTime() {
+    console.log("Hi");
+    var mountain = $("#mountainInput").val();
+    var home = $("#locationInput").val();
+    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" +
+      home +
+      "&destination=" + mountain + "&key=AIzaSyDRGTwL70MZMi0xfqWIPGWLTP1IdTHjHPA";
+    console.log(mountain)
+    console.log(home)
+
+    return $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(res => {
+      console.log(res)
+      console.log(res.routes[0].legs[0].duration.text);
+      var distance = res.routes[0].legs[0].duration.text
+
+      //display travel time in a card called resultsCard
+
+      var resultsCard = $("<div>");
+      resultsCard.addClass("card");
+
+      var d = $("<h5>").text(distance);
+
+      resultsCard.append(d);
+
+      //prepending the last search
+      $(".searchResult").prepend(resultsCard);
+    })
+  }
 
 });
